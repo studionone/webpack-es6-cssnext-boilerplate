942275457af56ba9d95f33a261ea766c5178d0a6
Josh Girvin <josh@jgirvin.com>
2015-06-14 14:05:47 +1000

    Init with WebPack instead of Browserify

---
+++ code/gulpfile.js
@@ -0,0 +1,8 @@
+/*eslint-disable*/
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    webpack = require('webpack'),
+    WebpackDevServer = require('webpack-dev-server');
+
+
+/*eslint-enable*/
